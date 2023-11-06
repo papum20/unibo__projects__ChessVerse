9b3c40ffe5f7313af6fd7e2a74fa90cf1886c37e
papum_debian <danieledugo1@gmail.com>
2023-11-06 11:47:01 +0100

    .

--- up.sh
+++ up.sh
@@ -3,6 +3,10 @@
 # environment
 set -a
 source .env
+source env/credentials.env
+
+# volumes
+mkdir -p ${VOLUME_MYSQL_DATA}
 
 # start
 docker compose $(ls docker-compose*.yml | awk '{printf "-f %s ", $0}') up -d
