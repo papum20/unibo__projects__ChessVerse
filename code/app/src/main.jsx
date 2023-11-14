3e5c08d1cc25853cf57f56b3774f820e0336e223
Giuseppe <giuseppspathis@gmail.com>
2023-11-14 13:38:12 +0100

    sistemate robe front end

--- code/app/src/main.jsx
+++ code/app/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
