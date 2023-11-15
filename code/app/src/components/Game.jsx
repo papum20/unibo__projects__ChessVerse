6d88386c599960fd2a3bf615871201931afe056b
Luca Gabellini <lgabellini1@gmail.com>
2023-11-15 12:47:50 +0100

    basic game

--- code/app/src/components/Game.jsx
+++ code/app/src/components/Game.jsx
@@ -3,7 +3,7 @@ import { Image, Row, Col, Card, Modal, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { Chessboard } from "react-chessboard";
 import { useEffect, useState } from "react";
-import { Chess } from 'chess.js';
+import Board from "./Board.jsx";
 import {createTheme,ThemeProvider} from '@mui/material/styles';
 import { Button  } from "@mui/material";
 import {Gear, Clock, ExclamationDiamond} from 'react-bootstrap-icons';
@@ -14,36 +14,6 @@ function Game (props) {
 
     const [moves, setMoves] = useState(["5a", "2b", "7f", "8g","5a", "2b", "7f", "8g","5a", "2b", "7f", "8g","5a", "2b", "7f", "8g",]);
     const [botMessages, setBotMessages] = useState(["ciao", "pippo", "pluto", "paperino","ciao", "pippo", "pluto", "paperino","ciao", "pippo", "pluto", "paperino",]);
-
-    const [game, setGame] = useState(new Chess());
-
-    function makeAMove(move) {
-        const gameCopy = { ...game };
-        const result = gameCopy.move(move);
-        setGame(gameCopy);
-        return result; // null if the move was illegal, the move object if the move was legal
-      }
-
-      function makeRandomMove() {
-        const possibleMoves = game.moves();
-        if (game.game_over() || game.in_draw() || possibleMoves.length === 0)
-          return; // exit if the game is over
-        const randomIndex = Math.floor(Math.random() * possibleMoves.length);
-        makeAMove(possibleMoves[randomIndex]);
-      }
-
-      function onDrop(sourceSquare, targetSquare) {
-        const move = makeAMove({
-          from: sourceSquare,
-          to: targetSquare,
-          promotion: "q", // always promote to a queen for example simplicity
-        });
-    
-        // illegal move
-        if (move === null) return false;
-        setTimeout(makeRandomMove, 200);
-        return true;
-      }
 
       const theme = createTheme({
         palette: {
@@ -159,7 +129,7 @@ function Game (props) {
                                 <Clock size={30}/>
                                 <p style={{marginLeft: "10px"}}>{time}</p>
                             </div>
-                            <Chessboard id="BasicBoard" position={game.fen()} onPieceDrop={onDrop} />
+                          <Board />
                         </div>
                         <div style={{display: "flex", justifyContent: "center", marginTop: "20px"}}>
                             <Button color="brown"   style={{fontSize: "1.5rem"}}  variant="contained" >Undo</Button>
