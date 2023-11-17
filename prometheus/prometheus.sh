b3a33ad35a5168a12559be3b477260b79f082128
papum_debian <danieledugo1@gmail.com>
2023-11-17 08:31:55 +0100

    restore prometheus

--- prometheus/prometheus.sh
+++ prometheus/prometheus.sh
@@ -43,7 +43,7 @@ docker run -d -t \
 	--name ${PROMETHEUS_AGENT_CONTAINER_NAME} \
 	-p ${PORT_EXPOSED}9090 \
 	-v ${WAL_DATA_DIR}:/prometheus \
-	-v /home/luca/t4-chessverse/prometheus/prometheus.yaml:/etc/prometheus/prometheus.yml \
+	-v ${PWD}/prometheus.yaml:/etc/prometheus/prometheus.yml \
 	--restart ${RESTART_POLICY} \
 	${PROMETHEUS_AGENT_IMAGE_NAME}
 	
