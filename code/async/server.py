4cb35bbafd6fb078a374e14d46a2cd1fb0481db9
papum_debian <danieledugo1@gmail.com>
2023-11-22 09:30:01 +0100

    async ->to websockets - CoAuthored: co-pilot

--- code/async/server.py
+++ code/async/server.py
@@ -3,172 +3,144 @@
 import asyncio
 import threading
 import os
-
-import socketio
-import aiohttp
-from time import perf_counter
-
+import json
 import chess
 import chess.engine
+import websockets
+from aiohttp import web
+from time import perf_counter
 
 from PVEGame import PVEGame
 
-sio = socketio.AsyncServer(async_mode='aiohttp', cors_allowed_origins='*', path=f'{os.environ.get("SUBPATH")}')
-app = aiohttp.web.Application()
-sio.attach(app)
+websocket_dict = {}
+
+async def handle_connect(websocket, _path=None):
+	sid = id(websocket)
+	print("connect ", sid)
+	# Store the websocket instance in a global variable or an appropriate data structure
+	websocket_dict[sid] = websocket
+
+async def handle_disconnect(websocket, _path):
+	sid = id(websocket)
+	print("disconnect ", sid)
+
+async def handle_start(websocket, data):
+	sid = id(websocket)
+	def check_int(key, min, max):
+		return key in data and isinstance(data[key], int) and min <= data[key] <= max
+	print("start ", sid, data)
+	if "rank" not in data or "depth" not in data or "time" not in data:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Missing fields", "fatal": True}}))
+		return
+	if not check_int("rank", 0, 100):
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Invalid rank", "fatal": True}}))
+		return
+	if not check_int("depth", 1, 20):
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Invalid bot strength", "fatal": True}}))
+		return
+	if not check_int("time", 1, 3000):
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Invalid clocktime", "fatal": True}}))
+		return
+	pveGames[sid] = PVEGame(sid, data["rank"], data["depth"], data["time"])
+	await pveGames[sid].initialize_bot()
+	await websocket.send(json.dumps({"event": "config", "data": {"fen": pveGames[sid].fen}}))
+
+async def handle_move(websocket, data):
+	sid = id(websocket)
+	print("move ", sid, data)
+	if sid not in pveGames:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Game not found", "fatal": True}}))
+		return
+	game = pveGames[sid]
+	if "san" not in data:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Missing fields"}}))
+		return
+	if not game.current.has_time():
+		return
+	try:
+		uci_move = game.board.parse_san(data["san"]).uci()
+	except (chess.InvalidMoveError, chess.IllegalMoveError):
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Invalid move"}}))
+		return
+	if chess.Move.from_uci(uci_move) not in game.board.legal_moves:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Invalid move"}}))
+		return
+	game.board.push_uci(uci_move)
+	outcome = game.board.outcome()
+	if outcome is not None:
+		await websocket.send(json.dumps({"event": "end", "data": {"winner": outcome.winner}}))
+		return
+	start = perf_counter()
+	bot_move = (await game.bot.play(game.board, chess.engine.Limit(time=game.time))).move
+	game.board.push_uci(bot_move.uci())
+	outcome = game.board.outcome()
+	latest_move = game.board.pop()
+	san_bot_move = game.board.san(bot_move)
+	game.board.push(latest_move)
+	if outcome is not None:
+		print(outcome)
+		await websocket.send(json.dumps({"event": "move", "data": {"san": san_bot_move}}))
+		await websocket.send(json.dumps({"event": "end", "data": {"winner": outcome.winner}}))
+		return
+	game.popped = False
+	end = perf_counter()
+	game.current.add_time(end - start)
+	game.current.first_move = False
+	await websocket.send(json.dumps({"event": "move", "data": {"san": san_bot_move}}))
+
+async def handle_resign(websocket, _data):
+	sid = id(websocket)
+	print("resign", sid)
+	if sid not in pveGames:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Game not found", "fatal": True}}))
+		return
+
+async def handle_pop(websocket, _data):
+	sid = id(websocket)
+	print("pop", sid)
+	if sid not in pveGames:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "Game not found", "fatal": True}}))
+		return
+	game = pveGames[sid]
+	if game.popped:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "You have already popped"}}))
+	elif game.board.fullmove_number == 1:
+		await websocket.send(json.dumps({"event": "error", "data": {"cause": "No moves to undo"}}))
+	else:
+		game.board.pop()
+		game.board.pop()
+		await websocket.send(json.dumps({"event": "pop", "data": {}}))
+		game.popped = True
 
 pveGames = {}
 
+async def cleaner(websocket_dict):
+	while True:
+		await asyncio.sleep(1)
 
-async def handle_connect(sid, _environ):
-    print("connect ", sid)
-
-
-async def handle_disconnect(sid):
-    print("disconnect ", sid)
-    if sid in pveGames.keys():
-        del pveGames[sid]
-
-
-async def handle_start(sid, data):
-    def check_int(key, min, max):
-        return key in data and isinstance(data[key], int) and min <= data[key] <= max
-    print("start ", sid, data)
-    if "rank" not in data or "depth" not in data or "time" not in data:
-        await sio.emit("error", {"cause": "Missing fields", "fatal": True}, room=sid)
-        return
-    if not check_int("rank", 0, 100):
-        await sio.emit("error", {"cause": "Invalid rank", "fatal": True}, room=sid)
-        return
-    if not check_int("depth", 1, 20):
-        await sio.emit("error", {"cause": "Invalid bot strength", "fatal": True}, room=sid)
-        return
-    if not check_int("time", 1, 3000):
-        await sio.emit("error", {"cause": "Invalid clocktime", "fatal": True}, room=sid)
-        return
-    pveGames[sid] = PVEGame(sid, data["rank"], data["depth"], data["time"])
-    await pveGames[sid].initialize_bot()
-    await sio.emit("config", {"fen": pveGames[sid].fen}, room=sid)
-
-
-async def handle_move(sid, data):
-    print("move ", sid, data)
-    if sid not in pveGames:
-        await sio.emit("error", {"cause": "Game not found", "fatal": True}, room=sid)
-        return
-    game = pveGames[sid]
-    if "san" not in data:
-        await sio.emit("error", {"cause": "Missing fields"}, room=sid)
-        return
-    if not game.current.has_time():
-        return
-    try:
-        uci_move = game.board.parse_san(data["san"]).uci()
-    except (chess.InvalidMoveError, chess.IllegalMoveError):
-        await sio.emit("error", {"cause": "Invalid move"}, room=sid)
-        return
-    if chess.Move.from_uci(uci_move) not in game.board.legal_moves:
-        await sio.emit("error", {"cause": "Invalid move"}, room=sid)
-        return
-    game.board.push_uci(uci_move)
-    outcome = game.board.outcome()
-    if outcome is not None:
-        await sio.emit("end", {"winner": outcome.winner}, room=sid)
-        await handle_disconnect(sid)
-        await sio.disconnect(sid)
-        return
-    start = perf_counter()
-    bot_move = (await game.bot.play(game.board, chess.engine.Limit(depth=game.depth))).move
-    game.board.push_uci(bot_move.uci())
-    outcome = game.board.outcome()
-    latest_move = game.board.pop()
-    san_bot_move = game.board.san(bot_move)
-    game.board.push(latest_move)
-    if outcome is not None:
-        print(outcome)
-        await sio.emit("move", {"san": san_bot_move}, room=sid)
-        await sio.emit("end", {"winner": outcome.winner}, room=sid)
-        await handle_disconnect(sid)
-        await sio.disconnect(sid)
-        return
-    game.popped = False
-    end = perf_counter()
-    game.current.add_time(end - start)
-    game.current.first_move = False
-    await sio.emit("move", {"san": san_bot_move}, room=sid)
-
-
-async def handle_resign(sid, _data):
-    print("resign", sid)
-    if sid not in pveGames:
-        await sio.emit("error", {"cause": "Game not found", "fatal": True}, room=sid)
-        return
-    await handle_disconnect(sid)
-    await sio.disconnect(sid)
-
-
-async def handle_pop(sid, _data):
-    print("pop", sid)
-    if sid not in pveGames:
-        await sio.emit("error", {"cause": "Game not found", "fatal": True}, room=sid)
-        return
-    game = pveGames[sid]
-    if game.popped:
-        await sio.emit("error", {"cause": "You have already popped"}, room=sid)
-    elif game.board.fullmove_number == 1:
-        await sio.emit("error", {"cause": "No moves to undo"}, room=sid)
-    else:
-        game.board.pop()
-        game.board.pop()
-        await sio.emit("pop", {}, room=sid)
-        game.popped = True
-
-
-sio.on("connect", handle_connect)
-sio.on("disconnect", handle_disconnect)
-sio.on("resign", handle_resign)
-sio.on("pop", handle_pop)
-sio.on("move", handle_move)
-sio.on("start", handle_start)
-
-
-def cleaner_thread():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    async def cleaner():
-        while True:
-            await asyncio.sleep(1)
-
-            for sid in list(pveGames.keys()):
-                if sid not in pveGames:
-                    continue
-
-                for player in pveGames[sid].players:
-                    if not player.has_time():
-                        print("gotcha ", sid)
-                        await sio.emit("timeout", {}, room=sid)
-                        await handle_disconnect(sid)
-                        await sio.disconnect(sid)
-
-    loop.run_until_complete(cleaner())
+		for sid, websocket in websocket_dict.items():
+			for player in pveGames[sid].players:
+				if not player.has_time():
+					print("gotcha ", sid)
+					await websocket.send(json.dumps({"event": "timeout", "data": {}}))
+					await handle_disconnect(websocket, None)
 
 async def main():
-    runner = aiohttp.web.AppRunner(app)
-    await runner.setup()
-    site = aiohttp.web.TCPSite(runner, "0.0.0.0", os.environ.get("PORT"))
-    thread = threading.Thread(target=cleaner_thread)
-    thread.start()
-    await site.start()
-    print(f"listening on {os.environ.get('IP')}:{os.environ.get('PORT')}") 
-    while True:
-        await asyncio.sleep(1)
+	app = web.Application()
+	app.add_routes([web.get('/ws/{_path}', handle_connect),
+					web.post('/ws/{_path}', handle_connect)])
+	
+	loop = asyncio.get_running_loop()
+	loop.create_task(cleaner(websocket_dict))
 
+	runner = web.AppRunner(app)
+	await runner.setup()
+	site = web.TCPSite(runner, "0.0.0.0", int(os.environ.get("PORT")))
+	await site.start()
+	print(f"listening on 0.0.0.0:{os.environ.get('PORT')}")
+
+	while True:
+		await asyncio.sleep(1)
 
 if __name__ == "__main__":
-    asyncio.run(main())
-
-"""
-    per eseguire: 
-        python3.12 server.py
-"""
+	asyncio.run(main())
