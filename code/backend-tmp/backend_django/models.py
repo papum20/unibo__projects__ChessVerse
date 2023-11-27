53472120221468387af488a523d69796e0971c69
ccirone <112408901+cono14@users.noreply.github.com>
2023-11-27 10:43:23 +0100

    aggiunto backend signup e login

--- code/backend-tmp/backend_django/models.py
+++ code/backend-tmp/backend_django/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 class Guest(models.Model):
     Username = models.CharField(max_length=255, unique=True)
     class Meta:
@@ -7,3 +7,15 @@ class Guest(models.Model):
         db_table = 'Guest'
         
         
+class RegisteredUsers(models.Model):
+    Username = models.CharField(max_length=255, unique=True)
+    Password = models.CharField(max_length=255)
+    GamesWon = models.IntegerField(default=0)
+    GameDraw = models.IntegerField(default=0)
+    GamesLost = models.IntegerField(default=0)
+    EloReallyBadChess = models.IntegerField(default=1000)
+    EloSecondChess = models.IntegerField(default=1000)
+    
+    
+    def __str__(self):
+        return self.Username
