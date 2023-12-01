0925a23fdea01d54a8d116a0d46c332e89690705
Francesco Licchelli <francesco.licchelli@studio.unibo.it>
2023-12-01 04:18:14 +0100

    now on SSL!

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-REMOTE_USER="kekko"
-REMOTE_HOST="chessverse.germanywestcentral.cloudapp.azure.com"
-REMOTE_DIR="~/t4-chessverse"
+REMOTE_USER="prod"
+REMOTE_HOST="chessverse.northeurope.cloudapp.azure.com"
+REMOTE_DIR="/home/prod/t4-chessverse"
 
 # Connessione SSH e esecuzione dei comandi
 ssh $REMOTE_USER@$REMOTE_HOST << EOF
   cd $REMOTE_DIR
+  git checkout origin/prod
   git pull
   docker rm $(docker ps -a -q)
-  sudo docker compose build
-  sudo docker compose up -d
+  docker compose build
+  docker compose up -d
 EOF
