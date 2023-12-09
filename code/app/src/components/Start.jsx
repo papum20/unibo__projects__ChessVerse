2c87de0af3aaaecfc9b81795e28fa106f0723408
Francesco Licchelli <francesco.licchelli@studio.unibo.it>
2023-12-09 03:16:50 +0100

    modificato url connessione server async

--- code/app/src/components/Start.jsx
+++ code/app/src/components/Start.jsx
@@ -49,8 +49,7 @@ function Start({
             setBotDiff(MIN_BOT_DIFF);
         setShowModal(false);
         setIsLoadingGame(true);
-        // const host = import.meta.env.VITE_ASYNC_HOST ?? "http://localhost:8080";
-        const host = "https://sbhbd9h0-8080.euw.devtunnels.ms/"
+        const host = import.meta.env.VITE_ASYNC_HOST ?? "http://localhost:8080";
         const secure = import.meta.env.VITE_NODE_ENV == "production";
         const options = { transports: ["websocket"], secure}
         setSocket(io(host, options));
